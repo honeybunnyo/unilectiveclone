2813e0bda59e27e4940cfe6a92299f391afff7ec
Jacqueline Ta <jxcquelineta@gmail.com>
2024-02-18 00:25:37 +1100

    added icon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-
+import {Button} from"@chakra-ui/react"
 function App() {
   return (
-    <>
-      hello
-    </>
+    <div>
+      <img src="/unilectives.svg" alt="Unilectives Logo" />
+      <img src="/bookopen.svg" alt="Book-open Logo" />''
+      <Button>
+        hello
+      </Button>
+    </div>
   )
 }
 
