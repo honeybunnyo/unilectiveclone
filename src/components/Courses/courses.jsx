558e7739242fa4280acf2f0a8f04fcdd246d9d0c
Jacqueline Ta <jxcquelineta@gmail.com>
2024-02-18 03:33:41 +1100

    added courses

---
+++ src/components/Courses/courses.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const courses = () => {
+  return (
+    <>
+    <Flex>
+        <Box>hi</Box>
+        <Box>hi</Box>
+        <Box>hi</Box>
+    </Flex>
+    <Flex>
+        <Box>hi</Box>
+        <Box>hi</Box>
+        <Box>hi</Box>
+    </Flex>
+  </>
+
+  )
+}
+
+export default courses
