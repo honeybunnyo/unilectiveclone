eac99ce5e0378bd9df6c492b7b0147acbae75be5
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2024-02-27 20:44:40 +1100

    Changed button

--- src/components/PageContent/searchbar.jsx
+++ src/components/PageContent/searchbar.jsx
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
-import { Flex, Button } from '@chakra-ui/react';
+import React, { useState } from "react";
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalFooter,
+} from "@chakra-ui/react";
 
 const searchbar = () => {
-    const [isSearchClicked, setIsSearchClicked] = useState(false);
-    const handleSearchClick = () => {
-        setIsSearchClicked(!isSearchClicked);
-    };
-
-    return (
-        <>
-            <Flex style={{ width: '97%', height: '40px', alignItems: 'center', padding: '8px', color: "#989EFF", borderRadius: '6px', cursor: 'pointer', border: '2px solid #989EFF' }} onClick={handleSearchClick}>
-                <img src="/search.svg" alt="Search Logo" style={{ marginRight: '16px' }} />
-                Search for a course e.g COMP1511
-            </Flex>
-            {isSearchClicked && (
-                <div style={{
-                    position: 'fixed',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: '200px',
-                    height: '100px',
-                    border: '1px solid black',
-                    backgroundColor: 'white',
-                    zIndex: '9999',
-                    textAlign: 'center',
-                    borderRadius: '10px',
-                    padding: '20px'
-                }}>
-                    <Button onClick={handleSearchClick}>Dismiss</Button>
-                </div>
-            )}
-        </>
-    );
-}
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleSearchClick = () => {
+    onOpen();
+  };
+  return (
+    <>
+      <Button
+        style={{
+          width: "97%",
+          height: "40px",
+          alignItems: "center",
+          padding: "8px",
+          color: "#989EFF",
+          borderRadius: "6px",
+          cursor: "pointer",
+          border: "2px solid #989EFF",
+        }}
+        onClick={handleSearchClick}
+      >
+        <img
+          src="/search.svg"
+          alt="Search Logo"
+          style={{ marginRight: "16px" }}
+        />
+        Search for a course e.g COMP1511
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} centerContent>
+        <ModalOverlay />
+        <ModalContent
+          style={{
+            top: "40%",
+          }}
+        >
+          <ModalHeader>Press dismiss to close</ModalHeader>
+          <ModalCloseButton />
+          <ModalFooter>
+            <Button mr={3} onClick={onClose}>
+              Dismiss
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 export default searchbar;
